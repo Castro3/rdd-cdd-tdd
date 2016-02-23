38837b422d81423dd5d1f4750bb5088419d3ec86
Mcas <mikko.castro@devinci.fr>
2016-02-23 15:54:18 +0100

    added test

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -13,21 +13,22 @@ var ip = '';
 
 describe('Config', function () {
     
+    var Config = new config(units, ip);
     it('has 3 props', function () {
-        var Config = new config(units, ip);
         expect(Config.args).to.exist;
         expect(Config.ip).to.exist;
         expect(Config.units).to.exist;
     });
 
     it('after construction, 2 props are populated', function () {
-        var Config = new config(units, ip);
         expect(Config.args).to.not.equal(null);
         expect(Config.units).to.not.equal(null);
         
     });
 
     it('has correct key types',function(){
-        
+        expect(Config.args).to.be.an('object');
+        expect(Config.units).to.be.an('object');
+        expect(Config.ip).to.be.an('string');
     });
 });
