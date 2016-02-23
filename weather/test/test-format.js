8ab83ea01ad47ce50040d753a6b7ef28538e07d5
Mcas <mikko.castro@devinci.fr>
2016-02-23 16:46:17 +0100

    Add test

--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -1,6 +1,9 @@
-var assert = require('assert'),
+var expect = require("chai").expect;
     format = require('./../lib/format');
 
 describe('format', function () {
-    it('is a constructor of the Format object')
+    it('is a constructor of the Format object',function()
+      {
+        expect(typeof(format) === 'function').to.be.true;
+    });
 });
